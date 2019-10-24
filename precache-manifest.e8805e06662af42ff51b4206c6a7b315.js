f1968bd42968b5b47fd3487a3fe8e3c37a504b8c
fernandabelenVL <fernanda.vegaleiva@gmail.com>
2019-10-23 21:28:42 -0300

    Updates

---
+++ precache-manifest.e8805e06662af42ff51b4206c6a7b315.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "286f42b00fdb897b6d1ebb4d422a9c82",
+    "url": "/recreando-pinterest/index.html"
+  },
+  {
+    "revision": "98459831fbe58a1c8201",
+    "url": "/recreando-pinterest/static/css/main.0824cb2e.chunk.css"
+  },
+  {
+    "revision": "4554912ba74ce3c052ce",
+    "url": "/recreando-pinterest/static/js/2.837ecd62.chunk.js"
+  },
+  {
+    "revision": "98459831fbe58a1c8201",
+    "url": "/recreando-pinterest/static/js/main.04e824a8.chunk.js"
+  },
+  {
+    "revision": "8f50dcb966bd881aa7da",
+    "url": "/recreando-pinterest/static/js/runtime-main.610570b8.js"
+  }
+]);
